bb1dcae3f79adca118ac34fe5f044ec54a6d2740
hgctmy <hgctomo@gmail.com>
2023-04-20 20:54:26 +0900

    add chapter6

--- chapter6/nlp100_54.py
+++ chapter6/nlp100_54.py
@@ -7,8 +7,3 @@ with open('model.pickle', mode='rb') as f:
 
 X = pd.read_table("train.feature.txt")
 print(np.max(lr.predict_proba(X), axis=1), lr.predict(X), sep="\n", file=f)
-
-'''
-[0.84208212 0.87286569 0.76032397 ... 0.34452535 0.82772827 0.7959898 ]
-['b' 'e' 'b' ... 'e' 'e' 'e']
-'''
