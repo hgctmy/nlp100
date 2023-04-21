1638ff4d5a0b83efc517a850722452d4567d128d
hgctmy <hgctomo@gmail.com>
2023-04-21 12:40:02 +0900

    modify

--- chapter2/nlp100_16.py
+++ chapter2/nlp100_16.py
@@ -5,8 +5,8 @@ n = int(sys.argv[1])  # 標準入力からnを受け取る
 df = pd.read_table("popular-names.txt", header=None)
 line = df.shape[0]//n  # n分割したときの１つのファイルの行数
 for i in range(n):
-    df.split = df.iloc[i*line:(i+1)*line]
-    df.split.to_csv(f"popular-names{i}.txt", sep="\t", header=False, index=False)
+    df_split = df.iloc[i*line:(i+1)*line]
+    df_split.to_csv(f"popular-names{i}.txt", sep="\t", header=False, index=False)
 
 # 13.4身近なライブラリに親しむ,p.172
 
