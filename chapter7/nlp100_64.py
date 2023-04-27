3dee47fbca613bd05945379c9f790cdf748277c0
hgctmy <hgctomo@gmail.com>
2023-04-27 12:07:04 +0900

    modify

--- chapter7/nlp100_64.py
+++ chapter7/nlp100_64.py
@@ -5,10 +5,11 @@ model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz
 with open("questions-words.txt", mode="r")as fr, open("questions-words.similar.txt", mode="w")as fw:
     for line in fr:
         if line[0] == ":":
-            fw.write(line + '\n')
+            fw.write(line)
         else:  # vec(2列目の単語) - vec(1列目の単語) + vec(3列目の単語)と類似度が高い単語とその類似度
+            line = line.rstrip()
             words = line.split()
-            vector = model[words[1]] - model[words[0]] + model[words[2]]
+            vector = model[words[1]] - model[words[0]] + model[words[2]]  # ベクトルを計算
             word, score = model.similar_by_vector(vector, topn=1)[0]
             # 元々の行の末に計算したベクトルと最も類似した単語とその類似度を追加したものをファイルに書き込む
             fw.write(' '.join([line, word, str(score)]) + '\n')
